fc20d8218b58ddd22392065a1428875cb028fda3
Aryan <aryankrishnan4b@gmail.com>
2026-08-09 17:41:57 +0530

    Update C++ to fix race conditions
    This PR resolves 2 bugs
    1. A race condition in test_handler.cpp where output is blank as tmp_out
    is not created yet.
    2. A minor bug in updates_handler.cpp where messages aren't correctly displayed.
    
    This fixes
    1. By adding an additional line to create an empty file to prevent the race
    2. Switching to non-blocking commands in updates_handler while simultaneously updating the error messages.
    
    Signed-off-by: Aryan <aryankrishnan4b@gmail.com>

--- src_cs/backend/settings/updates_handler.cpp
+++ src_cs/backend/settings/updates_handler.cpp
@@ -6,26 +6,8 @@ using json = nlohmann::json;
 
 int handle_update(struct mg_connection *conn, void *)
 {
-	int exit_code = system("/etc/init.d/update.sh");
-	std::string error = "";
-	switch (exit_code) {
-	case 1:
-		error = "Download timed out (are you connected to the internet?)";
-		break;
-	case 2:
-		error = "Download failed! (Are you connected to the internet?)";
-		break;
-	case 3:
-		error = "Update failed to unpack!";
-		break;
-	case 4:
-		error = "Failed to restart!";
-		break;
-	default:
-		error = "Update has successfully started! You may continue using suvriddhi OS and wait until the restart in ~10 minutes. If the restart doesn't happen, the system will update the next time SuvriddhiOS Reboots.";
-	}
-
-	json res = { { "error", error } };
+	int exit_code = system("/etc/init.d/update.sh &");
+	json res = { { "error", "Update has successfully started! You may continue using suvriddhi OS and wait until the restart in ~10 minutes. Do not turn off the device until it does automatically unless it has been more than 20 minutes, in which case you may turn it off." } };
 	SendResponse(conn, res.dump());
-	return 500; // Since system() blocks the main thread the only way someone would get here is in an error state.
+	return 200;
 }
